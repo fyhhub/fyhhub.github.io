07e4c62ad69d1f30a90d1c3054f9e7c397561ee2
fyhhub <38740836+fyhhub@users.noreply.github.com>
2023-02-01 02:05:48 +0000

    Deploying to gh-pages from  @ b129565ec882214ca7af676f8463ec9cb5ebe4f4 🚀

---
+++ assets/index.html.a211a0fd.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./plugin-vueexport-helper.2444895f.js";import{o as t,c as o,a as e,b as r}from"./app.6e599b3b.js";const c={},n=e("h1",{id:"babel",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#babel","aria-hidden":"true"},"#"),r(" Babel")],-1),s=[n];function _(d,i){return t(),o("div",null,s)}const f=a(c,[["render",_],["__file","index.html.vue"]]);export{f as default};
