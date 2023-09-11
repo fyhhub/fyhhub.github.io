c44ec6b61c4abf2b53ca22f84931138327aff7ef
fyhhub <38740836+fyhhub@users.noreply.github.com>
2023-09-11 07:53:50 +0000

    Deploying to gh-pages from  @ f14695c8a13d9d0765c0f16faaa82c9a7ac5ee8e 🚀

---
+++ assets/algorithm_二叉树_98.验证二叉搜索树.md.b86727aa.js
@@ -0,0 +1,63 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.3d945e71.js";const D=JSON.parse('{"title":"98.验证二叉搜索树","description":"","frontmatter":{},"headers":[],"relativePath":"algorithm/二叉树/98.验证二叉搜索树.md","filePath":"algorithm/二叉树/98.验证二叉搜索树.md","lastUpdated":1694418694000}'),p={name:"algorithm/二叉树/98.验证二叉搜索树.md"},o=l(`<h1 id="_98-验证二叉搜索树" tabindex="-1"><a href="https://leetcode-cn.com/problems/validate-binary-search-tree/" target="_blank" rel="noreferrer">98.验证二叉搜索树</a> <a class="header-anchor" href="#_98-验证二叉搜索树" aria-label="Permalink to &quot;[98.验证二叉搜索树](https://leetcode-cn.com/problems/validate-binary-search-tree/)&quot;">​</a></h1><h2 id="题解" tabindex="-1">题解 <a class="header-anchor" href="#题解" aria-label="Permalink to &quot;题解&quot;">​</a></h2><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for a binary tree node.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function TreeNode(val, left, right) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.left = (left===undefined ? null : left)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.right = (right===undefined ? null : right)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@param</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{TreeNode}</span><span style="color:#6A737D;"> </span><span style="color:#E1E4E8;">root</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@return</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{boolean}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">isValidBST</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> (</span><span style="color:#FFAB70;">root</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> lastVal </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">-</span><span style="color:#E1E4E8;">Number.MAX_VALUE;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">dfs</span><span style="color:#E1E4E8;">(</span><span style="color:#FFAB70;">root</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#6A737D;">// 没有节点也返回true</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (</span><span style="color:#F97583;">!</span><span style="color:#E1E4E8;">root) </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">true</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#6A737D;">// 如果左子树是合法的搜索树</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (</span><span style="color:#B392F0;">dfs</span><span style="color:#E1E4E8;">(root.left)) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 上次记录的值和当前节点比较 如果当前节点更大，说明是二叉搜索树，继续向右递归</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (root.val </span><span style="color:#F97583;">&gt;</span><span style="color:#E1E4E8;"> lastVal) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">        lastVal </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> root.val;</span></span>
+<span class="line"><span style="color:#E1E4E8;">        </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">dfs</span><span style="color:#E1E4E8;">(root.right);</span></span>
+<span class="line"><span style="color:#E1E4E8;">      } </span><span style="color:#F97583;">else</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">        </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">false</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">      }</span></span>
+<span class="line"><span style="color:#E1E4E8;">    } </span><span style="color:#F97583;">else</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">false</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">dfs</span><span style="color:#E1E4E8;">(root);</span></span>
+<span class="line"><span style="color:#E1E4E8;">};</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for a binary tree node.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function TreeNode(val, left, right) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.left = (left===undefined ? null : left)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.right = (right===undefined ? null : right)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@param</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{TreeNode}</span><span style="color:#6A737D;"> </span><span style="color:#24292E;">root</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@return</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{boolean}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#D73A49;">var</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">isValidBST</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> (</span><span style="color:#E36209;">root</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">var</span><span style="color:#24292E;"> lastVal </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">-</span><span style="color:#24292E;">Number.MAX_VALUE;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">dfs</span><span style="color:#24292E;">(</span><span style="color:#E36209;">root</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6A737D;">// 没有节点也返回true</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (</span><span style="color:#D73A49;">!</span><span style="color:#24292E;">root) </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">true</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6A737D;">// 如果左子树是合法的搜索树</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (</span><span style="color:#6F42C1;">dfs</span><span style="color:#24292E;">(root.left)) {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 上次记录的值和当前节点比较 如果当前节点更大，说明是二叉搜索树，继续向右递归</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (root.val </span><span style="color:#D73A49;">&gt;</span><span style="color:#24292E;"> lastVal) {</span></span>
+<span class="line"><span style="color:#24292E;">        lastVal </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> root.val;</span></span>
+<span class="line"><span style="color:#24292E;">        </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">dfs</span><span style="color:#24292E;">(root.right);</span></span>
+<span class="line"><span style="color:#24292E;">      } </span><span style="color:#D73A49;">else</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">        </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">false</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">      }</span></span>
+<span class="line"><span style="color:#24292E;">    } </span><span style="color:#D73A49;">else</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">false</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">dfs</span><span style="color:#24292E;">(root);</span></span>
+<span class="line"><span style="color:#24292E;">};</span></span></code></pre></div>`,3),e=[o];function t(r,c,y,E,i,A){return n(),a("div",null,e)}const f=s(p,[["render",t]]);export{D as __pageData,f as default};
