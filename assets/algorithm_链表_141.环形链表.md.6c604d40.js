7830e1e97ba38bc8a86ba7875c645e89396e6129
fyhhub <38740836+fyhhub@users.noreply.github.com>
2023-10-17 01:55:11 +0000

    Deploying to gh-pages from  @ fa6ce8ad8454ce834c90bea45f120bfbe8223360 🚀

---
+++ assets/algorithm_链表_141.环形链表.md.6c604d40.js
@@ -0,0 +1,45 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.3d945e71.js";const A=JSON.parse('{"title":"141.环形链表","description":"","frontmatter":{},"headers":[],"relativePath":"algorithm/链表/141.环形链表.md","filePath":"algorithm/链表/141.环形链表.md","lastUpdated":1697507502000}'),p={name:"algorithm/链表/141.环形链表.md"},o=l(`<h1 id="_141-环形链表" tabindex="-1"><a href="https://leetcode-cn.com/problems/linked-list-cycle/" target="_blank" rel="noreferrer">141.环形链表</a> <a class="header-anchor" href="#_141-环形链表" aria-label="Permalink to &quot;[141.环形链表](https://leetcode-cn.com/problems/linked-list-cycle/)&quot;">​</a></h1><h2 id="思路" tabindex="-1">思路 <a class="header-anchor" href="#思路" aria-label="Permalink to &quot;思路&quot;">​</a></h2><p>快慢指针，快指针每次走两步，慢指针每次走一步，如果有环就一定会相交</p><h2 id="题解" tabindex="-1">题解 <a class="header-anchor" href="#题解" aria-label="Permalink to &quot;题解&quot;">​</a></h2><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for singly-linked list.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function ListNode(val) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = val;</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.next = null;</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@param</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{ListNode}</span><span style="color:#6A737D;"> </span><span style="color:#E1E4E8;">head</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@return</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{boolean}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">hasCycle</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> (</span><span style="color:#FFAB70;">head</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> fast </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> head;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> slow </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> head;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// while循环保证至少有两个节点</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">while</span><span style="color:#E1E4E8;"> (fast </span><span style="color:#F97583;">&amp;&amp;</span><span style="color:#E1E4E8;"> fast.next) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    fast </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> fast.next.next;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    slow </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> slow.next;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (slow </span><span style="color:#F97583;">===</span><span style="color:#E1E4E8;"> fast) </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">true</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">false</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">};</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for singly-linked list.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function ListNode(val) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = val;</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.next = null;</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@param</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{ListNode}</span><span style="color:#6A737D;"> </span><span style="color:#24292E;">head</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@return</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{boolean}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#D73A49;">var</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">hasCycle</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> (</span><span style="color:#E36209;">head</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> fast </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> head;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> slow </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> head;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// while循环保证至少有两个节点</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">while</span><span style="color:#24292E;"> (fast </span><span style="color:#D73A49;">&amp;&amp;</span><span style="color:#24292E;"> fast.next) {</span></span>
+<span class="line"><span style="color:#24292E;">    fast </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> fast.next.next;</span></span>
+<span class="line"><span style="color:#24292E;">    slow </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> slow.next;</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (slow </span><span style="color:#D73A49;">===</span><span style="color:#24292E;"> fast) </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">true</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">false</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">};</span></span></code></pre></div>`,5),e=[o];function t(c,r,y,E,i,d){return n(),a("div",null,e)}const D=s(p,[["render",t]]);export{A as __pageData,D as default};
