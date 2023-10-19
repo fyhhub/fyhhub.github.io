6d48052dc2544d8672c06ba92102c99c0132cc9e
fyhhub <38740836+fyhhub@users.noreply.github.com>
2023-10-19 01:38:24 +0000

    Deploying to gh-pages from  @ c291c5ed87950f1f95859ee66181bbf14d057a21 🚀

---
+++ assets/algorithm_链表_206.反转链表.md.ed5f6e9c.js
@@ -0,0 +1,43 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.3d945e71.js";const D=JSON.parse('{"title":"206.反转链表","description":"","frontmatter":{},"headers":[],"relativePath":"algorithm/链表/206.反转链表.md","filePath":"algorithm/链表/206.反转链表.md","lastUpdated":1697679316000}'),p={name:"algorithm/链表/206.反转链表.md"},o=l(`<h1 id="_206-反转链表" tabindex="-1"><a href="https://leetcode-cn.com/problems/reverse-linked-list/" target="_blank" rel="noreferrer">206.反转链表</a> <a class="header-anchor" href="#_206-反转链表" aria-label="Permalink to &quot;[206.反转链表](https://leetcode-cn.com/problems/reverse-linked-list/)&quot;">​</a></h1><h2 id="题解" tabindex="-1">题解 <a class="header-anchor" href="#题解" aria-label="Permalink to &quot;题解&quot;">​</a></h2><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for singly-linked list.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function ListNode(val, next) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.next = (next===undefined ? null : next)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@param</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{ListNode}</span><span style="color:#6A737D;"> </span><span style="color:#E1E4E8;">head</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@return</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{ListNode}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">reverseList</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> (</span><span style="color:#FFAB70;">head</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> pre </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">null</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> cur </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> head;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">while</span><span style="color:#E1E4E8;"> (cur) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">const</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">next</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> cur.next;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    cur.next </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> pre;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    pre </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> cur;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    cur </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> next;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> pre;</span></span>
+<span class="line"><span style="color:#E1E4E8;">};</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for singly-linked list.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function ListNode(val, next) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.next = (next===undefined ? null : next)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@param</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{ListNode}</span><span style="color:#6A737D;"> </span><span style="color:#24292E;">head</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@return</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{ListNode}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#D73A49;">var</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">reverseList</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> (</span><span style="color:#E36209;">head</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> pre </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">null</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> cur </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> head;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">while</span><span style="color:#24292E;"> (cur) {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">const</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">next</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> cur.next;</span></span>
+<span class="line"><span style="color:#24292E;">    cur.next </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> pre;</span></span>
+<span class="line"><span style="color:#24292E;">    pre </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> cur;</span></span>
+<span class="line"><span style="color:#24292E;">    cur </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> next;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> pre;</span></span>
+<span class="line"><span style="color:#24292E;">};</span></span></code></pre></div>`,3),e=[o];function t(c,r,y,E,i,d){return n(),a("div",null,e)}const u=s(p,[["render",t]]);export{D as __pageData,u as default};
