0a77b8afa1b17325a6c181bb989262180cd3dd45
fyhhub <38740836+fyhhub@users.noreply.github.com>
2024-04-18 15:14:09 +0000

    Deploying to gh-pages from  @ d3c55e26e81e5deea132383db1b9848604d9c788 🚀

---
+++ assets/algorithm_二叉树_501.二叉搜索树中的众数.md.59f5453f.js
@@ -0,0 +1,101 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.3d945e71.js";const u=JSON.parse('{"title":"501.二叉搜索树中的众数","description":"","frontmatter":{},"headers":[],"relativePath":"algorithm/二叉树/501.二叉搜索树中的众数.md","filePath":"algorithm/二叉树/501.二叉搜索树中的众数.md","lastUpdated":1713453121000}'),p={name:"algorithm/二叉树/501.二叉搜索树中的众数.md"},o=l(`<h1 id="_501-二叉搜索树中的众数" tabindex="-1"><a href="https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/" target="_blank" rel="noreferrer">501.二叉搜索树中的众数</a> <a class="header-anchor" href="#_501-二叉搜索树中的众数" aria-label="Permalink to &quot;[501.二叉搜索树中的众数](https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/)&quot;">​</a></h1><h2 id="题解" tabindex="-1">题解 <a class="header-anchor" href="#题解" aria-label="Permalink to &quot;题解&quot;">​</a></h2><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for a binary tree node.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function TreeNode(val, left, right) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.left = (left===undefined ? null : left)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.right = (right===undefined ? null : right)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@param</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{TreeNode}</span><span style="color:#6A737D;"> </span><span style="color:#E1E4E8;">root</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#F97583;">@return</span><span style="color:#6A737D;"> </span><span style="color:#B392F0;">{number[]}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">findMode</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> (</span><span style="color:#FFAB70;">root</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> pre;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> count </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">0</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> maxCount </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">0</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> res </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> [];</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">fn</span><span style="color:#E1E4E8;">(</span><span style="color:#FFAB70;">root</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (</span><span style="color:#F97583;">!</span><span style="color:#E1E4E8;">root) </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#B392F0;">fn</span><span style="color:#E1E4E8;">(root.left);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (pre </span><span style="color:#F97583;">==</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">null</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 第一个节点</span></span>
+<span class="line"><span style="color:#E1E4E8;">      count </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">1</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    } </span><span style="color:#F97583;">else</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (pre </span><span style="color:#F97583;">===</span><span style="color:#E1E4E8;"> root.val) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 有相同节点</span></span>
+<span class="line"><span style="color:#E1E4E8;">      count</span><span style="color:#F97583;">++</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    } </span><span style="color:#F97583;">else</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 节点不同</span></span>
+<span class="line"><span style="color:#E1E4E8;">      count </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">1</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">    pre </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> root.val;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#6A737D;">// 与最大频率相同</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (count </span><span style="color:#F97583;">===</span><span style="color:#E1E4E8;"> maxCount) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      res.</span><span style="color:#B392F0;">push</span><span style="color:#E1E4E8;">(root.val);</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#6A737D;">// 如果当前节点出现的频率 大于 最大频率</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (count </span><span style="color:#F97583;">&gt;</span><span style="color:#E1E4E8;"> maxCount) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 更新最大频率</span></span>
+<span class="line"><span style="color:#E1E4E8;">      maxCount </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> count;</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#6A737D;">// 记录</span></span>
+<span class="line"><span style="color:#E1E4E8;">      res </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> [root.val];</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#B392F0;">fn</span><span style="color:#E1E4E8;">(root.right);</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#B392F0;">fn</span><span style="color:#E1E4E8;">(root);</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> res;</span></span>
+<span class="line"><span style="color:#E1E4E8;">};</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * Definition for a binary tree node.</span></span>
+<span class="line"><span style="color:#6A737D;"> * function TreeNode(val, left, right) {</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.val = (val===undefined ? 0 : val)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.left = (left===undefined ? null : left)</span></span>
+<span class="line"><span style="color:#6A737D;"> *     this.right = (right===undefined ? null : right)</span></span>
+<span class="line"><span style="color:#6A737D;"> * }</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#6A737D;">/**</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@param</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{TreeNode}</span><span style="color:#6A737D;"> </span><span style="color:#24292E;">root</span></span>
+<span class="line"><span style="color:#6A737D;"> * </span><span style="color:#D73A49;">@return</span><span style="color:#6A737D;"> </span><span style="color:#6F42C1;">{number[]}</span></span>
+<span class="line"><span style="color:#6A737D;"> */</span></span>
+<span class="line"><span style="color:#D73A49;">var</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">findMode</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> (</span><span style="color:#E36209;">root</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> pre;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> count </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">0</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> maxCount </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">0</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">let</span><span style="color:#24292E;"> res </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> [];</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">function</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">fn</span><span style="color:#24292E;">(</span><span style="color:#E36209;">root</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (</span><span style="color:#D73A49;">!</span><span style="color:#24292E;">root) </span><span style="color:#D73A49;">return</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6F42C1;">fn</span><span style="color:#24292E;">(root.left);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (pre </span><span style="color:#D73A49;">==</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">null</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 第一个节点</span></span>
+<span class="line"><span style="color:#24292E;">      count </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">1</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    } </span><span style="color:#D73A49;">else</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (pre </span><span style="color:#D73A49;">===</span><span style="color:#24292E;"> root.val) {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 有相同节点</span></span>
+<span class="line"><span style="color:#24292E;">      count</span><span style="color:#D73A49;">++</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    } </span><span style="color:#D73A49;">else</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 节点不同</span></span>
+<span class="line"><span style="color:#24292E;">      count </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">1</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"><span style="color:#24292E;">    pre </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> root.val;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6A737D;">// 与最大频率相同</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (count </span><span style="color:#D73A49;">===</span><span style="color:#24292E;"> maxCount) {</span></span>
+<span class="line"><span style="color:#24292E;">      res.</span><span style="color:#6F42C1;">push</span><span style="color:#24292E;">(root.val);</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6A737D;">// 如果当前节点出现的频率 大于 最大频率</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (count </span><span style="color:#D73A49;">&gt;</span><span style="color:#24292E;"> maxCount) {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 更新最大频率</span></span>
+<span class="line"><span style="color:#24292E;">      maxCount </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> count;</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6A737D;">// 记录</span></span>
+<span class="line"><span style="color:#24292E;">      res </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> [root.val];</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6F42C1;">fn</span><span style="color:#24292E;">(root.right);</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6F42C1;">fn</span><span style="color:#24292E;">(root);</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> res;</span></span>
+<span class="line"><span style="color:#24292E;">};</span></span></code></pre></div>`,3),e=[o];function c(t,r,E,y,i,A){return n(),a("div",null,e)}const F=s(p,[["render",c]]);export{u as __pageData,F as default};
