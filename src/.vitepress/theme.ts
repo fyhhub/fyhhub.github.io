1dfdef45993f996c51587020c12d85c0335f90a9
前端菜13 <38740836+fyhhub@users.noreply.github.com>
2026-03-28 14:15:54 +0800

    chore: 移除前端开发、计算机基础、后端开发、Rust、Python、留言板导航

--- src/.vitepress/theme.ts
+++ src/.vitepress/theme.ts
@@ -1,41 +1,6 @@
 export const nav = [
   { text: '掘金文章', link: 'https://juejin.cn/user/4406498334867783/posts' },
   { text: '开源', link: '/open-source/' },
-  {
-    text: '前端开发',
-    items: [
-      {
-        text: '前端基础',
-        link: '/frontend-basic/'
-      },
-      {
-        text: '前端进阶',
-        link: '/frontend-advanced/'
-      },
-      {
-        text: '前端工程化',
-        link: '/frontend-engineering/'
-      },
-      { text: '前端面经汇总', link: '/interview/' }
-    ]
-  },
-  {
-    text: '计算机基础',
-    items: [
-      {
-        text: '算法',
-        link: '/algorithm/'
-      },
-      {
-        text: '操作系统与网络',
-        link: '/basic/'
-      }
-    ]
-  },
-  { text: '后端开发', link: '/backend/' },
   { text: '开发工具', link: '/tools/' },
-  { text: '随笔', link: '/informal/' },
-  { text: 'Rust', link: '/rust-learn/' },
-  { text: 'Python', link: '/python/' },
-  { text: '留言板', link: '/comment/' }
+  { text: '随笔', link: '/informal/' }
 ];
